b995c055c6ccda4f2dec02da1122e844eb9a6224
yucong <923355209@qq.com>
2021-05-29 14:33:46 +0800

    update

--- websocket-demo/src/main/java/com/cn/websocketdemo/controller/WebSocketController.java
+++ websocket-demo/src/main/java/com/cn/websocketdemo/controller/WebSocketController.java
@@ -19,11 +19,12 @@ public class WebSocketController {
 
     /**
      * 群发消息内容
+     *
      * @param message
      * @return
      */
-    @RequestMapping(value="/sendAll", method= RequestMethod.GET)
-    public String sendAllMessage(@RequestParam(required=true) String message){
+    @RequestMapping(value = "/sendAll", method = RequestMethod.GET)
+    public String sendAllMessage(@RequestParam(required = true) String message) {
         try {
             WebSocketServer.BroadCastInfo(message);
         } catch (IOException e) {
@@ -34,14 +35,15 @@ public class WebSocketController {
 
     /**
      * 指定会话ID发消息
+     *
      * @param message 消息内容
-     * @param id 连接会话ID
+     * @param id      连接会话ID
      * @return
      */
-    @RequestMapping(value="/sendOne", method=RequestMethod.GET)
-    public String sendOneMessage(@RequestParam(required=true) String message,@RequestParam(required=true) String id){
+    @RequestMapping(value = "/sendOne", method = RequestMethod.GET)
+    public String sendOneMessage(@RequestParam(required = true) String message, @RequestParam(required = true) String id) {
         try {
-            WebSocketServer.SendMessage(message,id);
+            WebSocketServer.SendMessage(message, id);
         } catch (IOException e) {
             e.printStackTrace();
         }
