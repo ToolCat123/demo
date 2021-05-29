cf16e38e2d3f0bf79ff35557bce9854b8d085573
yucong <923355209@qq.com>
2021-05-29 17:34:12 +0800

    update

--- websocket-demo/src/main/java/com/cn/websocketdemo/controller/WebSocketController.java
+++ websocket-demo/src/main/java/com/cn/websocketdemo/controller/WebSocketController.java
@@ -1,8 +1,8 @@
 package com.cn.websocketdemo.controller;
 
 import com.cn.websocketdemo.service.WebSocketServer;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +21,7 @@ public class WebSocketController {
      *
      * @param message 消息
      */
-    @RequestMapping(value = "/sendAll", method = RequestMethod.GET)
+    @GetMapping(value = "/sendAll")
     public String sendAllMessage(@RequestParam String message) {
         try {
             WebSocketServer.broadCastInfo(message);
@@ -37,7 +37,7 @@ public class WebSocketController {
      * @param message 消息内容
      * @param id      连接会话ID
      */
-    @RequestMapping(value = "/sendOne", method = RequestMethod.GET)
+    @GetMapping(value = "/sendOne")
     public String sendOneMessage(@RequestParam String message, @RequestParam String id) {
         try {
             WebSocketServer.sendMessage(message, id);
